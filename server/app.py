36499ea75ae9baa0f142eceb3400133078bf34c0
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-09-28 14:33:35 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,48 +1,50 @@
 import os
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from flask import Flask, jsonify, request, make_response, render_template
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
+load_dotenv()
+
 from models import db, Bird
 
-app = Flask(
-    __name__,
-    static_url_path='',
-    static_folder='../client/build',
-    template_folder='../client/build'
-)
+app = Flask(__name__, static_url_path="", static_folder="../client/build", template_folder="../client/build")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://mybird_db_user:kh4HnGk68NKhqgWcIAqOh5YrgBDcuWCA@dpg-ckal8isldnhc73elu7c0-a.oregon-postgres.render.com/mybird_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template("index.html")
 
+
 api = Api(app)
 
-class Birds(Resource):
 
+class Birds(Resource):
     def get(self):
         birds = [bird.to_dict() for bird in Bird.query.all()]
         return make_response(jsonify(birds), 200)
 
     def post(self):
-
         data = request.get_json()
 
         new_bird = Bird(
-            name=data['name'],
-            species=data['species'],
-            image=data['image'],
+            name=data["name"],
+            species=data["species"],
+            image=data["image"],
         )
 
         db.session.add(new_bird)
@@ -50,16 +52,22 @@ class Birds(Resource):
 
         return make_response(new_bird.to_dict(), 201)
 
-api.add_resource(Birds, '/birds')
+
+api.add_resource(Birds, "/birds")
+
+
+@app.route("/")
+@app.route("/<int:id>")
+def index(id=0):
+    return render_template("index.html")
+
 
 class BirdByID(Resource):
-    
     def get(self, id):
         bird = Bird.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(bird), 200)
 
     def patch(self, id):
-
         data = request.get_json()
 
         bird = Bird.query.filter_by(id=id).first()
@@ -73,11 +81,11 @@ class BirdByID(Resource):
         return make_response(bird.to_dict(), 200)
 
     def delete(self, id):
-
         bird = Bird.query.filter_by(id=id).first()
         db.session.delete(bird)
         db.session.commit()
 
-        return make_response('', 204)
+        return make_response("", 204)
 
-api.add_resource(BirdByID, '/birds/<int:id>')
+
+api.add_resource(BirdByID, "/birds/<int:id>")
